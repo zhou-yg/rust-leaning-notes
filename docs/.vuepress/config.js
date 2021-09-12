ef6873e9d5c15f981c87dcf279a68d059118b123
zhouyg <yunge.zyg@alibaba-inc.com>
2021-09-12 12:21:01 +0800

    feat: init complete

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Github', link: 'https://github.com/zhou-yg/rust-leaning-notes' },
+    ],
+    sidebar: [
+      '/',
+      '/quick-start',
+      '/hello-world',
+    ]
+  },
+}
---
+++ docs/hello-world.md
@@ -0,0 +1,3 @@
+# hello world示例
+
+## abc
