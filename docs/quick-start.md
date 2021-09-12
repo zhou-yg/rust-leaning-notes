960ef1ec08302fa7de3b9c6344f927fa748fcf00
zhouyg <yunge.zyg@alibaba-inc.com>
2021-09-12 16:06:40 +0800

    feat: add rust  programe example

--- docs/quick-start.md
+++ docs/quick-start.md
@@ -2,7 +2,7 @@
 
 参考rust官网 <https://www.rust-lang.org/zh-CN/learn/get-started>
 
-## 1.在线运行
+# 1.在线运行
 
 主要新手初体验语言特性，在线跑代码，适合初步尝试
 
@@ -11,3 +11,9 @@
 安装
 
 > curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## 关于cargo
+
+综合了包管理 + 构建，类似npm + webpack的结合体，cargo主要依赖工程目录下的 Cargo.toml（类似package.json）
+
+相关文档 <https://doc.rust-lang.org/cargo/reference/manifest.html>
---
+++ rust/guess/guess-game.rs
@@ -0,0 +1,26 @@
+use std::io::stdin;
+use rand::Rng;
+
+fn main() {
+  println!("start");
+
+  let mut num = String::new();
+
+  stdin().read_line(&mut num);
+
+  println!("the input {}", num);
+
+  let secret = rand::thread_rng().gen_range(1..10);
+
+  println!("the input {}, s = {}", num, secret);
+
+  let guess: u32 = match num.trim().parse() {
+    Ok(num) => num,
+    Err(e) => {
+      println!("{}", e);
+      1
+    },
+  };
+
+    println!("the input {}, s = {}, parsed num = {}", num, secret, guess);
+}
